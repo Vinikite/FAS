f6e2ca8ceb4e5318341e7efb8e2c28fdfe234d6a
Nikita Shilov <vinikite35@gmail.com>
2017-06-13 19:35:48 +0300

    Q

---
+++ FAS.WebUI/Controllers/ChartDataRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FAS.WebUI.Controllers
+{
+    internal class ChartDataRepository
+    {
+        internal static object WorldScreenResolution()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
